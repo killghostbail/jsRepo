2d51f97990682baa68b274814923a4daa59b0c3c
killghostbail <killghostbail@gmail.com>
2023-01-26 19:13:43 +0300

    add: variables, console.log and alert.

---
+++ script.js
@@ -0,0 +1,10 @@
+const title = "First steps of learning JS";
+const screens = "BENQ";
+let screenPrice = 20000;
+let rollback = 10000;
+let fullPrice = 30000;
+let adaptive = 12345;
+
+alert("Hello, user!");
+
+console.log("Why do you check console on a blank page?");
